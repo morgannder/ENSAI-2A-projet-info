118f0330f0ee77db308c3f9392167fb3dd03430f
Morgann <morgann.derick@eleve.ensai.fr>
2025-10-07 15:11:55 +0200

    test

--- data/pop_db_test.sql
+++ data/pop_db_test.sql
@@ -1,8 +1,9 @@
 INSERT INTO utilisateur(id_joueur, pseudo, mdp, age, langue, est_majeur) VALUES
 (1, 'admin',      '0000',     1,      'Français',      false),
 (2, 'a',             'a',     20,     'Français',      true),
-(3, 'John',    '1234',     20,        'English',       true),
+(3, 'John',    '1234',        20,     'English',       true),
 (4, 'batricia',   '9876',     15,     'Italiano',      false),
-(5, 'Gilbert',     'abcd',     23,    'Deutsch',       true),
-(6, 'miguel',    'toto',     11,      'Español',       false),
-(7, 'junior',     'aaaa',     35,     'Deutsch',       true);
+(5, 'Gilbert',     'abcd',    23,     'Deutsch',       true),
+(6, 'miguel',    'toto',      11,     'Español',       false),
+(7, 'junior',     'aaaa',     35,     'Deutsch',       true),
+(8, 'junior',     'jsp',      35,     'Deutsch',       true);
